57aa554c7d9a5910cff547f6625fbfaa8ac2c9ac
vanieljekoek <134632970+vanieljekoek@users.noreply.github.com>
2023-05-28 12:42:59 +0300

    по замечаниям

--- main.py
+++ main.py
@@ -129,6 +129,83 @@ class BotInterface:
                     DatabaseConnection.disconnect_from_database(connection)
                 else:
                     self.message_send(event.user_id, 'Неверная команда')
+                    
+#     # Тестовый код, в случае если у пользователя не заполнены данные: пол или город или возраст
+#     def handle_missing_info(self, user_id):
+#         if self.params['sex'] is None:
+#             self.message_send(user_id, 'Укажите ваш пол ("Муж" или "Жен")')
+#         elif self.params['city'] is None:
+#             self.message_send(user_id, 'Укажите город, в котором вы проживаете')
+#         elif self.params['bdate'] is None:
+#             self.message_send(user_id, 'Укажите год вашего рождения')
+
+#     def handle_user_input(self, user_id, command):
+#         command = command.lower()
+#         if self.params['sex'] is None:
+#             if command == 'муж':
+#                 self.params['sex'] = 2
+#                 self.handle_missing_info(user_id)
+#             elif command == 'жен':
+#                 self.params['sex'] = 1
+#                 self.handle_missing_info(user_id)
+#             else:
+#                 self.message_send(user_id, 'Некорректный ввод. Укажите ваш пол ("Муж" или "Жен")')
+#         elif self.params['city'] is None:
+#             self.params['city'] = command
+#             self.handle_missing_info(user_id)
+#         elif self.params['bdate'] is None:
+#             try:
+#                 birth_year = int(command)
+#                 self.params['bdate'] = f"01.01.{birth_year}"
+#                 self.handle_missing_info(user_id)
+#             except ValueError:
+#                 self.message_send(user_id, 'Некорректный ввод. Укажите год вашего рождения (число)')
+#         else:
+#             self.message_send(user_id, 'Неверная команда')
+
+#     def handle_command(self, user_id, command):
+#         if command in ('привет', 'приветик', 'hello', 'шалом', 'салам', 'hi'):
+#             self.params = self.api.get_profile_info(user_id)
+#             self.handle_missing_info(user_id)
+#         elif command in ('поиск', 'search', 'следующие', 'next', 'go', 'ещё', 'поехали','давай','го', 'fire'):
+#             if self.params is None:
+#                 self.message_send(user_id, 'Прежде чем начать поиск, укажите необходимую информацию:')
+#                 self.handle_missing_info(user_id)
+#             else:
+#                 connection = DatabaseConnection.connect_to_database()
+#                 DatabaseConnection.create_table_found_users(connection)
+#                 users = self.api.search_users(self.params)
+#                 user = users.pop()
+#                 vk_id = str(user["id"])
+#                 if vk_id not in shown_users:
+#                     shown_users.add(vk_id)
+#                     num_user += 1
+#                     # Checking if a user exists in the DB
+#                     if DatabaseConnection.check_found_users(connection, vk_id):
+#                         self.message_send(user_id, f"{user['name']} уже есть в базе данных.")
+#                     else:
+#                         # Adding new users to the DB
+#                         DatabaseConnection.insert_data_found_users(connection, vk_id, 0)
+#                         photos_user = self.api.get_photos(user['id'])
+#                         attachment = ''
+#                         for num, photo in enumerate(photos_user):
+#                             attachment += f'photo{photo["owner_id"]}_{photo["id"]},'
+#                             if num == 2:
+#                                 break
+#                         self.message_send(user_id,
+#                                           f'''Знакомься, это - {user["name"]} \n
+# А вот ссылочка на страницу пользователя: https://vk.com/id{user["id"]}''',
+#                                           attachment=attachment
+#                                           )
+#             DatabaseConnection.disconnect_from_database(connection)
+        
+#         elif command in ('пока', 'bye'):
+#             self.message_send(user_id, 'Мне очень жаль, но результаты поиска будут очищены\nДо новых встреч\U0001F44B')
+#             connection = DatabaseConnection.connect_to_database()
+#             DatabaseConnection.remove_table_found_users(connection)
+#             DatabaseConnection.disconnect_from_database(connection)
+#         else:
+#             self.handle_user_input(user_id, command)
 
 if __name__ == '__main__':
     bot = BotInterface(community_token, access_token)
