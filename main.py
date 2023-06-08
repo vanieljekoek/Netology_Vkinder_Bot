e98965d820cde3494d6b874d269daf33cf7ffad3
vanieljekoek <134632970+vanieljekoek@users.noreply.github.com>
2023-06-08 17:57:52 +0300

    Main code UP

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Импортируем необходимые библиотеки и модули
 import os
 import datetime
 import json
@@ -10,21 +9,18 @@ from dotenv import load_dotenv
 from utils.database import DatabaseConnect
 from utils.buttons import create_keyboard
 
-#Загружаем переменные среды исполнения
 load_dotenv()
 
 # Задаём уровень логов
-log_dir = os.path.join(os.path.dirname(__file__), 'vkinder_Log')    # Указание названия папки с логами
+log_dir = os.path.join(os.path.dirname(__file__), 'vkinder_Log')
 
 if not os.path.exists(log_dir):
-    os.makedirs(log_dir)    # Создание папки с логами рядом с основным исполняемым кодом
+    os.makedirs(log_dir)
 
-# Задаём уровень логов (Уровень DEBUG - максимально подробный лог)
 logging.basicConfig(filename=os.path.join(log_dir, 'debug.log'), level=logging.DEBUG,
                     # Формат - Время, Дата, событие, сообщение)
                     format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-# Инициализация работы бота
 class VKinderBot:
     def __init__(self):
         self.group_auth = vk_api.VkApi(token=os.getenv('c_token'))
@@ -36,20 +32,19 @@ class VKinderBot:
         self.search_offset = -1
     print('\U0001F916 "Vkinder" запущен!', 'Для прекращения работы бота нажмите CTRL + C', sep='\n')
 
-    # Функция запуска бота
     def run(self):
         self.database.create_table()
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
                 command = event.text.lower()
                 sender = event.user_id
-                # Обработчик любой другой команды кроме ...
+                # Ранний выход из функции. Обработчик любой другой команды кроме перечисленных
                 if command not in ('привет', 'поиск', 'search', 'go', 'следующие', 'next', 'ещё'):
                     self.write_message(sender, 'Мне очень жаль, но результаты поиска будут очищены\nДо новых встреч\U0001F44B')
                     self.database.delete_table()
                     os.system('python ' + os.path.realpath(__file__))
                     continue
-                # Обработка команды "Привет"
+
                 elif command == 'привет':
                     # Процесс получения данных со страницы пользователя
                     info = self.user_auth.method('users.get', {'user_ids': sender, 'fields': 'first_name,last_name,sex,city,bdate'})
@@ -61,24 +56,38 @@ class VKinderBot:
                         'city': info[0]['city'],
                         'bdate': info[0]['bdate'],
                     }
-                    # Отправка приветственного сообщения
-                    self.write_message(sender, f'''Приветствую тебя, {user_info["first_name"]} {user_info["last_name"]}!\nЯ - бот знакомств в социальной сети ВК.
-Готов помочь тебе найти интересных людей и, возможно, новых друзей или даже вторую половинку.\n\nДавай начнем!
-Просто напиши мне "Поиск", чтобы увидеть первый результат, соответствующий твоим предпочтениям.
-Если тебе понравится кто-то из них, я смогу предоставить тебе ссылку на страницу пользователя и даже некоторые фотографии.\n
-С уважением,
-Бот знакомств Vkinder \U0001F498''', self.keyboard)
+                    if user_info["city"] is None:
+                        self.write_message(sender, f'Приветствую тебя, {user_info["first_name"]} {user_info["last_name"]}\n'
+                                           f'Для продолжения работы с ботом, требуется указать свой город')
+                        command = user_info["city"]
+                    elif user_info["sex"] is None:
+                        self.write_message(sender, f'Приветствую тебя, {user_info["first_name"]} {user_info["last_name"]}\n'
+                                           f'Для продолжения работы с ботом, требуется уточнить Ваш пол.\n'
+                                           f'направьте мне сообщение в формате "муж"  или "жен"')
+                        if command != 'муж': user_info["sex"] = 1
+                        user_info["sex"] = 2
+                    elif user_info["bdate"] is None:
+                        self.write_message(sender, f'Приветствую тебя, {user_info["first_name"]} {user_info["last_name"]}\n'
+                                           f'Для продолжения работы с ботом, требуется указать дату Вашего рождения в формате DD.MM.YYYY')
+                        command = user_info["bdate"]
+                    else:
+                        self.write_message(sender, f'Приветствую тебя, {user_info["first_name"]} {user_info["last_name"]}!\n'
+                                           f'Я - бот знакомств в социальной сети ВК.\n'
+                                           f'Готов помочь тебе найти интересных людей и, возможно, новых друзей или даже вторую половинку.\n\n'
+                                           f'Давай начнем!\nПросто напиши мне "Поиск", чтобы увидеть первый результат, соответствующий твоим '
+                                           f'предпочтениям.\nЕсли тебе понравится кто-то из них, я смогу предоставить тебе ссылку на страницу '
+                                           f'пользователя и даже некоторые фотографии.\nС уважением,\nБот знакомств Vkinder \U0001F498', self.keyboard)
+
                 # Обработка команды "Поиск" и вариаций   
                 elif command in ('поиск', 'search', 'go'):
                     self.search_offset = 0
                     self.search_users(sender)
                 # Обработка команды "Следующее" и вариаций    
                 elif command in ('следующие', 'next', 'ещё'):
-                    if self.search_offset < 0:
-                        self.write_message(sender, 'Сперва воспользуйтесь поиском, нажав соответствующую кнопку или введя команду "Поиск"',
-                                           self.keyboard)
-                    else:
-                        self.search_users(sender)
+                    if self.search_offset > 0: self.search_users(sender)
+                    self.write_message(sender,
+                                        'Сперва воспользуйтесь поиском, нажав соответствующую кнопку или введя команду "Поиск"',
+                                        self.keyboard)                      
 
                 # Алгоритм проверки пользователей по БД
                 elif hasattr(event, 'payload') and event.payload:
@@ -90,7 +99,7 @@ class VKinderBot:
                         self.database.save_vk_users(vk_id, vk_url)
                         self.write_message(event.user_id, f'Пользователь {vk_url} сохранен')
                             
-    # Функция отправки Кандидатов пользователю
+    # Отправки Кандидатов пользователю
     def write_message(self, sender, message, keyboard=None, attachment=None):
         try:
             keyboard_data = keyboard.get_keyboard()
@@ -102,44 +111,26 @@ class VKinderBot:
         
     # Функция поиска пользователей
     def search_users(self, sender):
-        try:
-            search_params = {'user_ids': sender, 'fields': 'bdate, sex, city'}
-            user_info = self.user_auth.method('users.get', search_params)
-            user_info = user_info[0]
-
-        # Отправка сообщения пользователю, в случае неполной информации в профиле
-        except vk_api.exceptions.ApiError as e:
-            self.write_message(sender, f'Произошла ошибка при получении информации о пользователе: {e}', self.keyboard)
-            return
-        # сообщение с просьбой указать дату рождения
-        if 'bdate' not in user_info:
-            self.write_message(sender, 'Не удалось получить дату рождения пользователя. Укажите дату рождения в настройках вашего профиля.')
-            return
-        # Сообщение с просьбой выбрать пол пользователя
-        if 'sex' not in user_info:
-            self.write_message(sender, 'Не удалось получить пол пользователя. Укажите пол в настройках вашего профиля.')
-            return
-        # Сообщение с просьбой указать город проживания пользователя
-        if 'city' == "" in user_info:
-            self.write_message(sender, 'Не удалось получить информацию о городе. Укажите ваш город в настройках профиля.')
-            return
-
+        search_params = {'user_ids': sender, 'fields': 'bdate, sex, city'}
+        user_info = self.user_auth.method('users.get', search_params)
+        user_info = user_info[0]
         user_age = self.calculate_age(user_info['bdate'])
         user_sex = user_info['sex']
         user_city = user_info['city']['id']
 
         try:
-            search_response = self.user_auth.method('users.search',
-                                                    {'count': 1000,
-                                                     'city': user_city,
-                                                     'sex': 1 if user_sex == 2 else 2,
-                                                     'status': 0,
-                                                     'status_list': [1, 6],
-                                                     'age_from': user_age,
-                                                     'age_to': user_age,
-                                                     'has_photo': 1,
-                                                     'fields': 'photo_max_orig, screen_name',
-                                                     'offset': self.search_offset})
+            search_response = self.user_auth.method(
+                'users.search',
+                {'count': 1000,
+                'city': user_city,
+                'sex': 1 if user_sex == 2 else 2,
+                'status': 0,
+                'status_list': [1, 6],
+                'age_from': user_age,
+                'age_to': user_age,
+                'has_photo': 1,
+                'fields': 'photo_max_orig, screen_name',
+                'offset': self.search_offset})
 
         except vk_api.exceptions.ApiError as e:
             self.write_message(sender, f'Произошла ошибка при поиске пользователей: {e}', self.keyboard)
@@ -153,21 +144,19 @@ class VKinderBot:
             if user_info[0]['is_closed']: continue
             if self.database.check_vk_users(vk_id):continue
             photos = self.get_top_photos(user)
-            message = f'''Знакомься, это - {user_info[0]["first_name"]} {user_info[0]["last_name"]}
-Ссылочка на страницу пользователя: {vk_url}\n
-В последний раз пользователь был онлайн:    {datetime.datetime.fromtimestamp(user_info[0]["last_seen"]["time"]).strftime("%d-%m-%Yг. в %H:%M")}\n\n
-А вот лучшие фото со страницы \U0001f929'''
+            message = (f'Знакомься, это - {user_info[0]["first_name"]} {user_info[0]["last_name"]}\nСсылочка на страницу пользователя: {vk_url}\n'
+            f'В последний раз пользователь был онлайн: {datetime.datetime.fromtimestamp(user_info[0]["last_seen"]["time"]).strftime("%d-%m-%Yг. в %H:%M")}'
+            f'\n\nА вот лучшие фото со страницы \U0001f929')
             self.write_message(sender, message, self.keyboard)
                 
             attachment = []
             for photo in photos:
-                attachment.append('photo{}_{}'.format(photo['owner_id'], photo['id']))
+                attachment.append(f"photo{photo['owner_id']}_{photo['id']}")
             self.write_message(sender, '', self.keyboard, attachment=','.join(attachment))
             self.database.save_vk_users(vk_id, vk_url)
             break
         self.search_offset+=10
         
-    # Функция вычисления возраста пользователя
     def calculate_age(self, bdate):
         if bdate:
             bdate = datetime.datetime.strptime(bdate, '%d.%m.%Y')
@@ -177,7 +166,7 @@ class VKinderBot:
                     and datetime.datetime.now().day < bdate.day):
                 age = min(max(how_old - 1, how_old - 1), how_old + 1)
             return age
-    # Функция получения ТОП5 Фото
+    # Функция получения ТОП5 Фото со страницы кандидата по количеству лайков и комментариев
     def get_top_photos(self, user):
         photos_response = self.user_auth.method('photos.get', {'owner_id': user['id'], 'album_id': 'profile', 'rev': 1,
                                                                'count': 5, 'extended': 1, 'photo_sizes': 1})
@@ -191,12 +180,10 @@ class VKinderBot:
             photos_data.append(photo_data)
         photos_data = sorted(photos_data, key=lambda x: (x['likes'], x['comments']), reverse=True)[:5]
         return photos_data
-    
-    # Функция удаления таблицы с пользователями
+
     def del_table(self):
         self.database.delete_table()
         self.database.disconnect()
 
 bot = VKinderBot()
-bot.__init__()
 bot.run()
